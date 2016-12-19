4aeb66db6b8655ce5c05fa0941d52dfe312b2a15
Akritas Akritidis <akritasak@gmail.com>
2016-12-19 18:29:34 +0200

    Fixed param tags

--- parts/Qs.q.js
+++ parts/Qs.q.js
@@ -4,7 +4,7 @@
  * Returns an element with a specific id.
  * Starts for QueryId.
  *
- * @param {string} the id
+ * @param {string} id the id
  * @return {Element} the element, or null
  */
 function qi (id) {
@@ -15,7 +15,7 @@ function qi (id) {
  * Returns an element that match the specified selectors.
  * Starts for Query.
  *
- * @param {string} the selectors
+ * @param {string} text the selectors
  * @return {Element} the element, or null
  */
 function q (text) {
@@ -26,7 +26,7 @@ function q (text) {
  * Returns all the elements that match the specified selectors.
  * Starts for QueryAll.
  *
- * @param {string} the selectors
+ * @param {string} text the selectors
  * @return {Element[]} the array of the elements
  */
 function qa (text) {
@@ -38,8 +38,8 @@ function qa (text) {
  * element.
  * Starts for QuerySub.
  *
- * @param {Element} the parrent element
- * @param {string} the selectors
+ * @param {Element} sub the parrent element
+ * @param {string} text the selectors
  * @return {Element} the element, or null
  */
 function qs (sub, text) {
@@ -51,8 +51,8 @@ function qs (sub, text) {
  * parrent element.
  * Starts for QueryAllSub.
  *
- * @param {Element} the parrent element
- * @param {string} the selectors
+ * @param {Element} sub the parrent element
+ * @param {string} text the selectors
  * @return {Element[]} the array of the elements
  */
 function qas (sub, text) {
