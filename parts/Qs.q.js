c879815215eaa1a55c5cb9124235810213f0aff5
Akritas Akritidis <akritasak@gmail.com>
2016-12-19 18:13:18 +0200

    Create Qs.q.js

---
+++ parts/Qs.q.js
@@ -0,0 +1,60 @@
+// Queries
+
+/**
+ * Returns an element with a specific id.
+ * Starts for QueryId.
+ *
+ * @param {string} the id
+ * @return {Element} the element, or null
+ */
+function qi (id) {
+  return document.getElementById(id)
+}
+
+/**
+ * Returns an element that match the specified selectors.
+ * Starts for Query.
+ *
+ * @param {string} the selectors
+ * @return {Element} the element, or null
+ */
+function q (text) {
+  return document.querySelector(text)
+}
+
+/**
+ * Returns all the elements that match the specified selectors.
+ * Starts for QueryAll.
+ *
+ * @param {string} the selectors
+ * @return {Element[]} the array of the elements
+ */
+function qa (text) {
+  return document.querySelectorAll(text)
+}
+
+/**
+ * Returns an element that match the specified selectors inside a given parrent
+ * element.
+ * Starts for QuerySub.
+ *
+ * @param {Element} the parrent element
+ * @param {string} the selectors
+ * @return {Element} the element, or null
+ */
+function qs (sub, text) {
+  return sub.querySelector(text)
+}
+
+/**
+ * Returns all the elements that match the specified selectors inside a given
+ * parrent element.
+ * Starts for QueryAllSub.
+ *
+ * @param {Element} the parrent element
+ * @param {string} the selectors
+ * @return {Element[]} the array of the elements
+ */
+function qas (sub, text) {
+  return sub.querySelectorAll(text)
+}
