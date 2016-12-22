1c7c46cd26c2484fa678d2dbad61c62dd6714054
Akritas Akritidis <akritasak@gmail.com>
2016-12-22 10:49:39 +0200

    Added qacls and qascls

--- parts/Qs.c.js
+++ parts/Qs.c.js
@@ -20,3 +20,38 @@ function cls (ele, add, remove) {
     ele.classList.remove(remove[i])
   }
 }
+
+/**
+ * Adds and removes classes for each element that match the specified selectors.
+ * Stands for QueryAllCLaSses.
+ *
+ * @param {string} text the selectors
+ * @param {string[]} add the classes to add
+ * @param {string[]} remove the classes to remove
+ */
+function qacls (text, add, remove) {
+  var l = document.querySelectorAll(text)
+  var count = l.length
+  for (var i = 0; i < count; i += 1) {
+    cls(l[i], add, remove)
+  }
+}
+
+/**
+ * Adds and removes classes for each element that match the specified selectors
+ * inside a given parrent element.
+ * Stands for QueryAllSubCLaSses.
+ *
+ * @param {Element} sub the parrent element
+ * @param {string} text the selectors
+ * @param {string[]} add the classes to add
+ * @param {string[]} remove the classes to remove
+ */
+function qascls (sub, text, add, remove) {
+  var l = sub.querySelectorAll(text)
+  var count = l.length
+  for (var i = 0; i < count; i += 1) {
+    cls(l[i], add, remove)
+  }
+}
+
